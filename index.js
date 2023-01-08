c8273751a038f00a8f9b7621fbc10f093099402c
Maxime Verstraete <maxime.verstraete@student.howest.be>
2023-01-08 18:55:27 +0100

    test

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const API_URL = 'https://vrt-api-app.herokuapp.com/';
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
     next();
 });
 
